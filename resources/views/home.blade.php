81b19a21864538d8f3e62eca10d03d523c55c08d
thoriq_aziz <thoriq.aziz@technosoft.co.id>
2020-05-15 13:11:16 +0700

    Create view for login, register, & home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,45 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
+<div class="wrapper">
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in!
-                </div>
-            </div>
+    <!-- /#sidebar-wrapper -->
+    <nav id="sidebar">
+        <div class="sidebar-header">
+            <h3>Dashboard</h3>
         </div>
+        
+        <ul class="list-unstyled">
+          <li>
+              <a href="#"> Add New Customer</a>
+          </li>
+          <li>
+              <a href="#"> All Customers</a>
+          </li>
+          <li>
+			<a href="{{ route('logout') }}" onclick="event.preventDefault();
+            document.getElementById('logout-form').submit();"> Logout</a>
+            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                @csrf
+            </form>
+          </li>
+      </ul>
+    </nav>
+
+    <!-- Page Content -->
+    <div id="page-content-wrapper">
+      <nav class="navbar navbar-light bg-light mt-4 border-bottom border-secondary">
+        <div class="navbar-brand">Title</div>
+        <div>
+            <input class="form-control mr-sm-2" id="searchTerm" placeholder="Search...">
+        </div>
+      </nav>
+
+      <div class="container-fluid pt-4">
+        
+      </div>
     </div>
-</div>
+    <!-- /#page-content-wrapper -->
+
+  </div>
 @endsection
